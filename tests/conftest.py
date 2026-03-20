7a4e350e334f92dfa601f10d6bfc6d35fe8fdf76
Your Friendly Robotic SRE <oawofolu@redhat.com>
2026-03-19 20:41:44 -0400

    Updates

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,13 +6,13 @@ def pytest_addoption(parser):
     parser.addoption(
         "--test_data",
         action="store",
-        default="./tmp/temp/test_data.csv",
+        default="./temp/test_data.csv",
         help='Specify test data file path.'
     )
     parser.addoption(
         "--test_results",
         action="store",
-        default="./tmp/temp/test_results.csv",
+        default="./temp/test_results.csv",
         help='Specify test results file path.'
     )
 
